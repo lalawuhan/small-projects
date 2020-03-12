ec98a4664f99c91455c16c1933cd721ea7c5b5de
lalawuhan <precious.segoe@gmail.com>
2020-03-12 20:28:51 +0100

    created more secure production bundle

--- smallapp/index.js
+++ smallapp/index.js
@@ -2,12 +2,76 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { pool } = require("./config");
-
+const helmet = require("helmet");
+const compression = require("compression");
+const rateLimit = require("express-rate-limit");
+const { body, check } = require("express-validator");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(compression());
+app.use(helmet());
+
+/* const isProduction = process.env.NODE_ENV === "production";
+const origin = {
+    origin: isProduction ? "https://www.example.com" : "*"
+};
+app.use(cors(origin));
+ */
+
+//To help protect against brute force/DDoS attacks, we can limit the amount of requests using express-rate-limit.
+const limiter = rateLimit({
+    windowsMs: 1 * 60 * 1000, // 1 minute
+    max: 5 //5 requests
+});
+//With app.use(), it will apply to every endpoint, but we can also make certain endpoints stricter with another rateLimit.
+app.use(limiter);
+
+const postLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 1
+});
+
+//We can use express-validator to ensure any incoming request is valid, otherwise display an error.
+app.post(
+    "/books",
+    [
+        check("author")
+            .not()
+            .isEmpty()
+            .isLength({ min: 5, max: 255 })
+            .trim(),
+        check("title")
+            .not()
+            .isEmpty()
+            .isLength({ min: 5, max: 255 })
+            .trim()
+    ],
+    postLimiter,
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        const { author, title } = req.body;
+        pool.query(
+            "INSERT INTO books(author,title) VALUES ($1, $2)",
+            [author, title],
+            error => {
+                if (error) {
+                    throw error;
+                }
+                res.status(201).json({
+                    status: "success",
+                    message: "Book added."
+                });
+            }
+        );
+    }
+);
 
 const getBooks = (req, res) => {
     pool.query("SELECT * FROM books", (error, results) => {
@@ -17,9 +81,9 @@ const getBooks = (req, res) => {
         res.status(200).json(results.rows);
     });
 };
+
 const addBook = (req, res) => {
     const { author, title } = req.body;
-
     pool.query(
         "INSERT INTO books(author, title) VALUES ($1, $2)",
         [author, title],
@@ -30,6 +94,13 @@ const addBook = (req, res) => {
             res.status(201).json({ status: "success", message: "Book added" });
         }
     );
+};
+const deleteBook = (req, res) => {
+    if (!req.header("apiKey") || req.header("apiKey") !== process.env.API_KEY) {
+        return res
+            .status(401)
+            .json({ status: "error", message: "Unauthorized" });
+    }
 };
 
 app.route("/books")
