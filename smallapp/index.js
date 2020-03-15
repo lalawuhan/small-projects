fa6daf2ed6a6792f490ec8fe59dc6401a76256cc
lalawuhan <precious.segoe@gmail.com>
2020-03-15 21:44:33 +0100

    api test

--- smallapp/index.js
+++ smallapp/index.js
@@ -21,7 +21,7 @@ const origin = {
 app.use(cors(origin));
  */
 //To help protect against brute force/DDoS attacks, we can limit the amount of requests using express-rate-limit.
-const limiter = rateLimit({
+limiter = rateLimit({
     windowsMs: 1 * 60 * 1000, // 1 minute
     max: 5 //5 requests
 });
@@ -34,79 +34,37 @@ const postLimiter = rateLimit({
 });
 
 //We can use express-validator to ensure any incoming request is valid, otherwise display an error.
-app.post(
-    "/books",
-    [
-        check("author")
-            .not()
-            .isEmpty()
-            .isLength({ min: 5, max: 255 })
-            .trim(),
-        check("title")
-            .not()
-            .isEmpty()
-            .isLength({ min: 5, max: 255 })
-            .trim()
-    ],
-    postLimiter,
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        }
-
-        const { author, title } = req.body;
-        pool.query(
-            "INSERT INTO books(author,title) VALUES ($1, $2)",
-            [author, title],
-            error => {
-                if (error) {
-                    throw error;
-                }
-                res.status(201).json({
-                    status: "success",
-                    message: "Book added."
-                });
-            }
-        );
-    }
-);
-
-const getBooks = (req, res) => {
+const getBooks = (request, response) => {
     pool.query("SELECT * FROM books", (error, results) => {
         if (error) {
             throw error;
         }
-        res.status(200).json(results.rows);
+        response.status(200).json(results.rows);
     });
 };
 
-const addBook = (req, res) => {
-    const { author, title } = req.body;
+const addBook = (request, response) => {
+    const { author, title } = request.body;
+
     pool.query(
-        "INSERT INTO books(author, title) VALUES ($1, $2)",
+        "INSERT INTO books (author, title) VALUES ($1, $2)",
         [author, title],
         error => {
             if (error) {
                 throw error;
             }
-            res.status(201).json({ status: "success", message: "Book added" });
+            response
+                .status(201)
+                .json({ status: "success", message: "Book added." });
         }
     );
-};
-const deleteBook = (req, res) => {
-    if (!req.header("apiKey") || req.header("apiKey") !== process.env.API_KEY) {
-        return res
-            .status(401)
-            .json({ status: "error", message: "Unauthorized" });
-    }
 };
 
 app.route("/books")
     //GET endpoint
     .get(getBooks)
     //POST endpoint
-    .post(addBook);
+    .post(postLimiter, addBook);
 
 //Lets start this server!
 app.listen(process.env.PORT || 3002, () => {
--- smallapp/init.sql
+++ smallapp/init.sql
@@ -3,10 +3,5 @@ CREATE TABLE books (
   author VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL
 );
-
-INSERT INTO books (author, title)
-VALUES  ('Tsitsi Dangarembga', 'Nervous Conditions');
-VALUES  ('Angela Makholwa', 'The Blessed Girl');
-VALUES  ('Thirteen Months of Sunrise', 'Rania Mamoun');
-VALUES  ('Transparent City', 'Ondjaki');
+INSERT INTO books (author, title) VALUES  ('Tsitsi Dangarembga', 'Nervous Conditions');
 
